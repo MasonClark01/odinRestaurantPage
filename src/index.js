1085662519bb4c0efde3e082dfa421605a06cd73
Mason Clark <109944632+MasonClark01@users.noreply.github.com>
2024-01-22 22:09:08 -0800

    FinishedBasicStyling&AddedJStoSwitchContent

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("works!")
+let currentTab = document.getElementById("activeTab")
+let contentDisplay = document.getElementById("activeContent")
+let navTabs = document.querySelectorAll(".navTab")
+
+// Create the content for the different pages
+//Home
+let homeContent = document.createElement("div")
+let homeImage = document.createElement("img")
+homeImage.src = "./src/images/brockChef.webp"
+let homeText = document.createElement("p")
+homeText.innerText = "TESTING THE TEXT PORTION OF HOME PAGE TEST TEST TEST GALADE SIGMA CALCULATOR ATTACK ON TITAN"
+homeContent.appendChild(homeImage)
+homeContent.appendChild(homeText)
+//
+for(let nt of navTabs){
+    nt.addEventListener("click", function() {
+        setActiveTab(this)
+    })
+}
+
+function setActiveTab(selectedTab){
+    contentDisplay.innerHTML = ""
+    console.log(selectedTab)
+    if(selectedTab.innerHTML === "Home"){
+        contentDisplay.appendChild(homeContent)
+        selectedTab.id = "activeTab"
+    }
+}
